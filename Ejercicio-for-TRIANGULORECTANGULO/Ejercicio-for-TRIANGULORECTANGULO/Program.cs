7877f24b5aa6488cd3d0affeb78963aaa655d030
KDavid616 <124007266+KDavid616@users.noreply.github.com>
2023-03-09 23:15:06 -0600

    Add files via upload

---
+++ Ejercicio-for-TRIANGULORECTANGULO/Ejercicio-for-TRIANGULORECTANGULO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Introduzca un numero entero: ");
+        int alturaT = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= alturaT; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
+
